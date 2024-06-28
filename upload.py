22af5e56afd188924424cb2197486c9ad63ae326
Nickobellic <avrahulkanna17@gmail.com>
2024-06-28 22:04:23 +0530

    Update Comments

--- upload.py
+++ upload.py
@@ -106,7 +106,7 @@ def auth():
   # The file token.json stores the user's access and refresh tokens, and is
   # created automatically when the authorization flow completes for the first
   # time.
-  if os.path.exists("token.json"):
+  if os.path.exists("token.json"):  # Get it by following Google Drive API Docs
     creds = Credentials.from_authorized_user_file("token.json", SCOPES)
   # If there are no (valid) credentials available, let the user log in.
   if not creds or not creds.valid:
